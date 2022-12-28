0cab35a58c1ac4d395cc1292f92b070922857158
Adam Terebiński <aterebinski@wp.pl>
2022-12-28 13:49:10 +0100

    first commit

---
+++ KSDictionaryEditor/AsciiConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSDictionaryEditor
+{
+    class AsciiConverter
+    {
+        public static string HEX2ASCII(string hex)
+
+        {
+
+            string res = String.Empty;
+
+            for (int a = 0; a < hex.Length; a = a + 2)
+
+            {
+
+                string Char2Convert = hex.Substring(a, 2);
+
+                int n = Convert.ToInt32(Char2Convert, 16);
+
+                char c = (char)n;
+
+                res += c.ToString();
+
+            }
+
+            return res;
+
+        }
+
+
+
+        public static string ASCIITOHex(string ascii)
+
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(ascii);
+
+            foreach (byte b in inputBytes)
+
+            {
+
+                sb.Append(string.Format("{0:x2}", b));
+
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
--- KSDictionaryEditor/MainWindow.xaml.cs
+++ KSDictionaryEditor/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,185 @@ namespace KSDictionaryEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        FbConnection connection;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            connection = new FbConnection(@"Server=localhost;User=gabinet;Password=flavamed;Database=C:\KS\KS-PLW\BAZY\med1250.KSB");
+
+            ShowPersonel(Personel_Left);
+            ShowDictionaries(Dictonaries_Left);
+            //ShowPersonel(Personel_Right);
+        }
+
+        public void ShowPersonel(ListView panel)
+        {
+            try
+            {
+                string query = "select id, imie, nazw from prac where del=0";
+                FbDataAdapter adapter = new FbDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                panel.ItemsSource = table.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ShowPersonel: "+ex.ToString());
+            }
+            
+            
+        }
+
+        public void ShowDictionaries(ListView panel)
+        {
+            ListView lvPersonel;
+            CheckBox checkSharedDictonaries;
+            
+
+            switch (panel.Name)
+            {
+                default:
+                    lvPersonel = Personel_Left;
+                    checkSharedDictonaries = SharedDictionaries_Left;
+                    break;
+                case "DictionariesLeft":
+                    lvPersonel = Personel_Left;
+                    checkSharedDictonaries = SharedDictionaries_Left;
+                    break;
+            }
+
+            try
+            {
+                string query = "select u.kod, u.id as u_id,  u.nazw as usluga," +
+                " w.id as w_id,  w.nazw as wzorzec," +
+                " s.id as s_id, s.nazw as slownik, s.opis," +
+                " p.imie||' '||p.nazw as pracownik" +
+                " from uslg u" +
+                " join wzfo w on w.iduslg = u.id" +
+                " join slow s on s.idwzfo = w.id" +
+                " join prac p on p.id = s.idprac" +
+                " where w.del = 0 and s.del = 0 and s.usun = 0" +
+                " and s.idprac in (";
+
+                FbCommand command = new FbCommand(query, connection);
+
+
+
+                if (checkSharedDictonaries.IsChecked == true)
+                {
+                    query = query + "0,";
+                }
+                if (lvPersonel.SelectedItems.Count > 0)
+                {
+                    //command.Parameters.AddWithValue()
+                    foreach (DataRowView item in lvPersonel.SelectedItems)
+                    {
+                        query = query + item["id"].ToString() + ",";
+                    }
+
+
+                }
+                query = query + "-99) order by usluga, wzorzec, slownik";
+
+                command.CommandText = query;
+
+                //MessageBox.Show(query);
+                Trace.WriteLine(query);
+
+
+                FbDataAdapter adapter = new FbDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                panel.ItemsSource = table.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ShowDictionaries: " + ex.ToString());
+            }
+
+            
+        }
+
+        private void ShowElements(ListView panel)
+        {
+
+            try
+            {
+                DataRowView drv = Dictonaries_Left.SelectedItem as DataRowView;
+                if (drv != null)
+                {
+                    string id = drv["S_ID"].ToString();
+
+                    string query = "select * from slow where id = @id";
+                    FbCommand command = new FbCommand(query, connection);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    FbDataAdapter adapter = new FbDataAdapter(command);
+
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    string info = table.Rows[0].Field<string>("opis").ToString();
+
+                    /*
+                    string[] podzielone = info.Split(
+                            new string[] { "\r\n", "\r", "\n" }, 
+                            StringSplitOptions.None
+                        );
+                        */
+
+                    string[] podzielone = info.Split(
+                            new string[] { AsciiConverter.HEX2ASCII("0D0A"), AsciiConverter.HEX2ASCII("0D"), AsciiConverter.HEX2ASCII("0A") },
+                            StringSplitOptions.None
+                        );
+
+                    foreach (string item in podzielone)
+                    {
+                        string newItem = item.Replace(AsciiConverter.HEX2ASCII("CB99"), AsciiConverter.HEX2ASCII("0D0A"));
+                    }
+
+                    //MessageBox.Show(info);
+                    MessageBox.Show(podzielone[0]);
+
+                    panel.ItemsSource = table.DefaultView;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("showSlow: " + ex.ToString());
+            }
+            
+
+            //MessageBox.Show("Id = " + id);
+        }
+
+        private void Personel_Left_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowDictionaries(Dictonaries_Left);
+        }
+
+        private void SharedDictionaries_Left_Click(object sender, RoutedEventArgs e)
+        {
+            ShowDictionaries(Dictonaries_Left);
+        }
+
+        private void ClearPersonel_Left_Click(object sender, RoutedEventArgs e)
+        {
+            Personel_Left.UnselectAll();
+            ShowDictionaries(Dictonaries_Left);
+        }
+
+        private void AllPersonel_Left_Click(object sender, RoutedEventArgs e)
+        {
+            Personel_Left.SelectAll();
+            ShowDictionaries(Dictonaries_Left);
+        }
+
+        private void Dictonaries_Left_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowElements(Items_Left);
         }
     }
 }
